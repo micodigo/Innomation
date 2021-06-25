1ca8d6864f585cc1348baca8425bbd48ec68d88e
Mayank Pal <palmayank5494@gmail.com>
2021-06-25 20:43:22 +0530

    latest code

--- src/pages/ContactUs/SubComponents/Map.js
+++ src/pages/ContactUs/SubComponents/Map.js
@@ -21,3 +21,6 @@ const MapContainer = () => {
   );
 };
 export default MapContainer;
+
+
+{/* <div class="mapouter"><div class="gmap_canvas"><iframe width="600" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=28.66759273966348,%2077.30435149753099&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://2piratebay.org">pirate bay</a><br><style>.mapouter{position:relative;text-align:right;height:500px;width:600px;}</style><a href="https://www.embedgooglemap.net">embed google maps website</a><style>.gmap_canvas {overflow:hidden;background:none!important;height:500px;width:600px;}</style></div></div> */}
