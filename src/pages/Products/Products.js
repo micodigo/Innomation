47faeb3545687e09097ebb0105eb7150a7a39f46
Mayankpal22 <palmayank5494@gmail.com>
2021-11-06 18:37:07 +0530

    Product pic add

--- src/pages/Products/Products.js
+++ src/pages/Products/Products.js
@@ -2,29 +2,34 @@ import React from "react";
 import Portfolio from "../../components/Portfolio";
 import hero from "./../Services/Images/3.png";
 import Hero from "../../components/CommonHero";
+import HMI from "./Image/HMIs.jpg";
+import ISP1 from "./Image/IndustrialSpareparts1.jpg";
+import PA from "./Image/PanelAccessories.jpg";
+import PLC from "./Image/PLCs.png";
+import Sensor from "./Image/Sensors.jpeg";
 
 function Products() {
   const data = {
     services: [
       {
         name: "Machine Automation",
-        image: "xyz",
+        image: HMI,
       },
       {
         name: "Machine Automation",
-        image: "xyz",
+        image: ISP1,
       },
       {
         name: "Machine Automation",
-        image: "xyz",
+        image: PA,
       },
       {
         name: "Machine Automation",
-        image: "xyz",
+        image: PLC,
       },
       {
         name: "Machine Automation",
-        image: "xyz",
+        image: Sensor,
       },
     ],
   };
