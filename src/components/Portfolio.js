10573dfec16501576c68b8010e29a3d777f165b0
Mayank <palmayank5494@gmail.com>
2022-10-07 21:46:15 +0530

    Portfolio change services industries  and products changes

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -2,7 +2,7 @@ import React from "react";
 import cls from "./Portfolio.module.scss";
 
 function Portfolio(props) {
-  console.log(props.hero);
+  // console.log(props.hero);
   return (
     <>
       <div
@@ -11,33 +11,48 @@ function Portfolio(props) {
       >
         {/* <img src={props.hero} alt="" /> */}
       </div>
-      {props.data.services.map((element, index) => (
-        <div key={index}>
-          {index % 2 === 0 ? (
-            <div className={`${cls.objects} ${cls.object1}`}>
-              <div style={{ display: "inline-block", border: "2px solid red" }}>
-                {element.name}
+      <div className={cls.section}>
+        {props.data.services.map((element, index) => (
+          <div key={index}>
+            {index % 2 === 0 ? (
+              <div className={`${cls.objects} ${cls.object1}`}>
+                {/* <div> */}
+                <img src={element.image} alt="" className={cls.div1Images} />
+                <h2>{element.name}</h2>
+                {/* </div> */}
+                <div className={cls.TextDetails}>
+                  <h3>{element.name}</h3>
+                  <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                    Asperiores, quam? Lorem ipsum dolor sit amet consectetur
+                    adipisicing elit. Laudantium, voluptatum.
+                  </p>
+                </div>
               </div>
-              <div style={{ display: "inline-block" }}>
+            ) : (
+              <div className={`${cls.objects} ${cls.object2}`}>
+                {/* <div style={{ border: "2px solid blue" }}> */}
                 <img src={element.image} alt="" />
+                <h2>{element.name}</h2>
+                {/* </div> */}
+                <div className={cls.TextDetails}>
+                  <h3>{element.name}</h3>
+                  <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                    Asperiores, quam? Lorem ipsum dolor sit amet consectetur
+                    adipisicing elit. Laudantium, voluptatum.
+                  </p>
+                </div>
               </div>
-            </div>
-          ) : (
-            <div className={`${cls.objects} ${cls.object2}`}>
-              <div style={{ display: "inline-block" }}>
-                <img src={element.image} alt="" />
-              </div>
-              <div
-                style={{ display: "inline-block", border: "2px solid blue" }}
-              >
-                {element.name}
-              </div>
-            </div>
-          )}
-        </div>
-      ))}
+            )}
+          </div>
+        ))}
+      </div>
     </>
   );
 }
 
 export default Portfolio;
+
+// style={{ display: "inline-block" }}
+// style={{ display: "inline-block", border: "2px solid blue" }}
