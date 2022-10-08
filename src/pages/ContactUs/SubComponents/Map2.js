15a0e41c091c991f68eba71a39668e30aef1eb24
Mayank <palmayank5494@gmail.com>
2022-10-08 13:18:22 +0530

    ContactUs Changes and responsiveness added

--- src/pages/ContactUs/SubComponents/Map2.js
+++ src/pages/ContactUs/SubComponents/Map2.js
@@ -3,17 +3,15 @@ import cls from "./Map2.module.scss";
 
 function Map2() {
   return (
-    <div className={cls.mapouter}>
-      <div>
-        <iframe
-          //   width="1092"
-          //   height="400"
-          //   id="gmap_canvas"
-          className={cls.map}
-          title="Map"
-          src="https://maps.google.com/maps?q=InnoMation%20Tech%20Services&t=&z=16&ie=UTF8&iwloc=&output=embed"
-        />
-      </div>
+    <div>
+      <iframe
+        //   width="1092"
+        //   height="400"
+        //   id="gmap_canvas"
+        className={cls.map}
+        title="Map"
+        src="https://maps.google.com/maps?q=InnoMation%20Tech%20Services&t=&z=16&ie=UTF8&iwloc=&output=embed"
+      />
     </div>
   );
 }
